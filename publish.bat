b6f01c26a7edafcbf8fcb4f819847ed03f552334
Rick Ellis <riccello@yahoo.com>
2021-08-01 20:08:16 -0500

    2nd deployment attempt

--- publish.bat
+++ publish.bat
@@ -2,11 +2,6 @@
 set /p msg="Enter git commit message: "
 @echo on
 
-@REM cd example
-@REM call yarn unlink "@reactiff/ui-core"
-@REM cd..
-@REM call yarn unlink
-
 git add .
 git commit -m "%msg%"
 git push
