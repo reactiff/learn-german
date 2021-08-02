c5cdf85c39ce2dae44e704a461792d226ecd40b1
Rick Ellis <riccello@yahoo.com>
2021-08-01 20:03:07 -0500

    First deployment

---
+++ publish.bat
@@ -0,0 +1,17 @@
+@echo off
+set /p msg="Enter git commit message: "
+@echo on
+
+@REM cd example
+@REM call yarn unlink "@reactiff/ui-core"
+@REM cd..
+@REM call yarn unlink
+
+git add .
+git commit -m "%msg%"
+git push
+
+yarn deploy
+
+echo
+echo Done!
